395b2522721439d02e1a5efb05212a884d00a09e
HooraProjects <hoora88.mehdipour@gmail.com>
2023-06-09 16:47:55 +0330

    add header footer variables in js

--- script.js
+++ script.js
@@ -2,11 +2,36 @@
 const menuIcon = document.querySelector('.menu-icon');
 const sideBar = document.querySelector('.right--header--menu');
 const closeIcon = document.querySelectorAll('.close-icon');
-const overlay = document.querySelector('.overlay')
+const overlay = document.querySelector('.overlay');
+const footer  = `<div class="footer">
+
+<div class="wpb-wrapper">
+<div class="links-footer">
+  <a href="#">جواهرات آرما</a>
+  <a href="#">تضمین برگشت / معاوضه کالا</a>
+  <a href="#">باشگاه مشتریان</a>
+  <a href="#">درباره ما / تماس با ما</a>
+</div>
+
+<div><small>تمامی حقوق مادی و معنوی این وبسایت متعلق به جواهری آرما می‌باشد</small></div>
+</div>
+
+<div class="wpb-wrapper">
+<div class="join-club-footer-title"><p>عضویت در خبرنامه</p></div>
+<div class="join-club-footer">
+  <input type="email" placeholder="ایمیل شما">
+<input type="submit" value="عضویت">
+</div>
+
+</div></div>`;
+
+document.querySelectorAll('footer').forEach(function(f){
+    f.innerHTML= footer;
+})
 overlay.style.display="none"
  sideBar.style.display = "none";
  
-  
+
 
 menuIcon.addEventListener('click',function(){
     
