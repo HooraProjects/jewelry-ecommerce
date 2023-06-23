ea8bb4b493922e7e23144bfe71a006ce718a3265
Hoora <88542810+HooraProjects@users.noreply.github.com>
2023-06-23 19:00:35 +0330

    remove some dupilcate codes

--- script.js
+++ script.js
@@ -13,15 +13,92 @@ const rangeSlider = document.getElementById('myRange');
 const orderby = document.querySelector('.orderby');
 const product  = document.querySelector('.product');
 
+const menuIcon = document.querySelectorAll('.menu-icon');
+ const sideBar = document.querySelector('.right--header--menu');
+const overlay = document.querySelector('.overlay');
 
-const openMenu = function(){
-  sideBar.style.display = "flex";
-  overlay.style.display ="block";
-  overlay.addEventListener('click',function(){
-    sideBar.style.display = "none";
-    overlay.style.display="none";
+
+const header = ` <nav class="right-header">
+<a class="menu-icon"><i class="fa-solid fa-bars"></i></a>
+<ul class="right--header--menu">
+  <li>
+    <a class="close-icon"><i class="fa-solid fa-xmark"></i></a>
+  </li>
+  <li><a href="#" class="menu-item active" id="menu-item-1">آرما</a></li>
+  <li>
+    <a href="./armaShop.html" class="menu-item" id="menu-item-2">فروشگاه</a>
+  </li>
+  <li>
+    <a href="./armaBlog.html" class="menu-item" id="menu-item-3">وبلاگ</a>
+  </li>
+  <li>
+    <a href="./armaAboutUs.html" class="menu-item" id="menu-item-4"
+      >درباره ما</a
+    >
+  </li>
+  <li>
+    <a href="./armaContactUs.html" class="menu-item" id="menu-item-5"
+      >تماس با ما</a
+    >
+  </li>
+</ul>
+</nav>
+
+<img src="./assets/images/logo.png" id="logo--header" />
+<div class="left-header">
+<a href="#" class="menu-item-left-header"
+  ><i class="fa-solid fa-magnifying-glass"></i
+></a>
+<a href="#" class="menu-item-left-header"
+  ><i class="fa-solid fa-cart-shopping"></i
+></a>
+</div>`;
+const footer  = `<div class="footer" style="display: flex;
+
+justify-content: space-around;
+background-color: #F6F6F8;
+height: 150px;
+width: 100%;">
+
+<div class="wpb-wrapper">
+<div class="links-footer">
+<a href="#" class="links-footer-item">جواهرات آرما</a>
+<a href="#" class="links-footer-item">تضمین برگشت / معاوضه کالا</a>
+<a href="#" class="links-footer-item">باشگاه مشتریان</a>
+<a href="#" class="links-footer-item">درباره ما / تماس با ما</a>
+</div>
+
+<div><small>تمامی حقوق مادی و معنوی این وبسایت متعلق به جواهری آرما می‌باشد</small></div>
+</div>
+
+<div class="wpb-wrapper">
+<div class="join-club-footer-title"><p>عضویت در خبرنامه</p></div>
+<div class="join-club-footer">
+<input type="email" placeholder="ایمیل شما">
+<input type="submit" value="عضویت" class="btn-submit-footer" >
+</div>
+
+</div></div>`;
+sideBar.style.display = "none";
+
+  overlay.style.display="none"
+
+
+
+
+function openMenu(){
+  menuIcon.addEventListener('click',function(){
+    
+   sideBar.style.display = "flex";
+    overlay.style.display ="block";
+    overlay.addEventListener('click',function(){
+      sideBar.style.display = "none";
+      overlay.style.display="none";
+    })
   })
 }
+
+
 
 closeIcon.forEach(function(icon){
   icon.addEventListener('click',function(){
@@ -45,116 +122,11 @@ document.querySelectorAll('header').forEach(function(h){
 })
 
  
-
-
-
-
-
-
-
-
-   
-
-       
-           
-            let products = [
-                {
-                      id:1,
-                      src:'./assets/images/necklace-shop-lazy.png',
-                      dataSrc:'./assets/images/necklace.jpg',
-                      name:'گردنبند سه زنجیره',
-                      price:'۵۵۰۰۰۰۰۰',
-                      popularity:1,
-                      rating:5
-
-                },
-                {
-                    id:2,
-                    src:'./assets/images/earring2-shop-lazy.jpg',
-                    dataSrc:'/assets/images/earring2-shop.jpg',
-                    name:'گوشواره مروارید طلا زرد',
-                    price:'۲۵۰۰۰۰۰۰',
-                    popularity:5,
-                    rating:3
-                },
-                {
-                    id:3,
-                    src:'./assets/images/necklace2-shop-lazy.jpg',
-                    dataSrc:'./assets/images/necklace2-shop.jpg',
-                    name:'گردنبند سه زنجیره با سنگ فیروزه',
-                    price:'۶۵۰۰۰۰۰۰',
-                    popularity:3,
-                    rating:4
-                },
-                {
-                    id:4,
-                    src:'./assets/images/earring1-shop-lazy.jpg',
-                    dataSrc:'./assets/images/earring1-shop.jpg',
-                    name:'گوشواره طرح گل',
-                    price:'۲۰۰۰۰۰۰۰',
-                    popularity:4,
-                    rating:4
-                },
-                {
-                    id:5,
-                    src:'./assets/images/ring1-shop-lazy.jpg',
-                    dataSrc:'./assets/images/ring1-shop.jpg',
-                    name:'انگشتر دوحلقه ای',
-                    price:'۳۵۰۰۰۰۰۰',
-                    popularity:2,
-                    rating:5
-                },
-                {
-                    id:6,
-                    src:'./assets/images/rings1-shop-lazy.jpg',
-                    dataSrc:'./assets/images/rings1-shop.jpg',
-                    name:'ست حلقه طلا سفید',
-                    price:'۷۰۰۰۰۰۰۰',
-                    popularity:6,
-                    rating:3
-                },
-                {
-                    id:7,
-                    src:'./assets/images/bracelet2-shop-lazy.jpg',
-                    dataSrc:'./assets/images/bracelet1-shop.jpg',
-                    name:'دستبند طلا زرد و طلا سفید',
-                    price:'۴۰۰۰۰۰۰۰',
-                    popularity:7,
-                    rating:4
-                },
-                
-                {
-                    id:8,
-                    src:'./assets/images/rings2-shop-lazy.jpg',
-                    dataSrc:'./assets/images/rings2-shop.jpg',
-                    name:'ست حلقه طلا زرد و طلا سفید',
-                    price:'۷۵۰۰۰۰۰۰',
-                    popularity:8,
-                    rating:2
-                },
-                {
-                    id:9,
-                    src:'./assets/images/bracelet2-shop-lazy.jpg',
-                    dataSrc:'./assets/images/bracelet2-shop.jpg',
-                    name:'دستبند طلا زرد',
-                    price:'۶۰۰۰۰۰۰۰',
-                    popularity:9,
-                    rating:1
-                }
-               
-            ]
               
             
-            menuIcon.addEventListener('click',function(){
-                
-                 sideBar.style.display = "flex";
-                 overlay.style.display="block";    
-                 overlay.addEventListener('click',function(){
-        sideBar.style.display = "none";
-        overlay.style.display="none";
-      })
+        
                        
-            })
+          
             closeIcon.forEach(function(icon){
                 icon.addEventListener('click',function(){
                     sideBar.style.display = "none";
@@ -167,73 +139,9 @@ document.querySelectorAll('header').forEach(function(h){
                 
             // })
       // lazy loading images
-      'use strict';
-      const menuIcon = document.querySelector('.menu-icon');
-      const sideBar = document.querySelector('.right--header--menu');
       
-      const overlay = document.querySelector('.overlay');
       
-      const header = ` <nav class="right-header">
-      <a class="menu-icon"><i class="fa-solid fa-bars"></i></a>
-      <ul class="right--header--menu">
-        <li>
-          <a class="close-icon"><i class="fa-solid fa-xmark"></i></a>
-        </li>
-        <li><a href="#" class="menu-item active" id="menu-item-1">آرما</a></li>
-        <li>
-          <a href="./armaShop.html" class="menu-item" id="menu-item-2">فروشگاه</a>
-        </li>
-        <li>
-          <a href="./armaBlog.html" class="menu-item" id="menu-item-3">وبلاگ</a>
-        </li>
-        <li>
-          <a href="./armaAboutUs.html" class="menu-item" id="menu-item-4"
-            >درباره ما</a
-          >
-        </li>
-        <li>
-          <a href="./armaContactUs.html" class="menu-item" id="menu-item-5"
-            >تماس با ما</a
-          >
-        </li>
-      </ul>
-      </nav>
-      
-      <img src="./assets/images/logo.png" id="logo--header" />
-      <div class="left-header">
-      <a href="#" class="menu-item-left-header"
-        ><i class="fa-solid fa-magnifying-glass"></i
-      ></a>
-      <a href="#" class="menu-item-left-header"
-        ><i class="fa-solid fa-cart-shopping"></i
-      ></a>
-      </div>`;
-      const footer  = `<div class="footer" style="display: flex;
-      
-      justify-content: space-around;
-      background-color: #F6F6F8;
-      height: 150px;
-      width: 100%;">
-      
-      <div class="wpb-wrapper">
-      <div class="links-footer">
-      <a href="#" class="links-footer-item">جواهرات آرما</a>
-      <a href="#" class="links-footer-item">تضمین برگشت / معاوضه کالا</a>
-      <a href="#" class="links-footer-item">باشگاه مشتریان</a>
-      <a href="#" class="links-footer-item">درباره ما / تماس با ما</a>
-      </div>
-      
-      <div><small>تمامی حقوق مادی و معنوی این وبسایت متعلق به جواهری آرما می‌باشد</small></div>
-      </div>
-      
-      <div class="wpb-wrapper">
-      <div class="join-club-footer-title"><p>عضویت در خبرنامه</p></div>
-      <div class="join-club-footer">
-      <input type="email" placeholder="ایمیل شما">
-      <input type="submit" value="عضویت" class="btn-submit-footer" >
-      </div>
-      
-      </div></div>`;
+     
       
       document.querySelectorAll('footer').forEach(function(f){
           f.innerHTML= footer;
@@ -241,27 +149,15 @@ document.querySelectorAll('header').forEach(function(h){
       document.querySelectorAll('header').forEach(function(h){
           h.innerHTML= header;
       })
-      overlay.style.display="none"
-       sideBar.style.display = "none";
+     
        
       
+        
+      
+  
+        
       
       
-      
-      
-      
-      for (const menu of document.querySelectorAll('.menu-icon')){
-          menu.addEventListener('click',function(){
-              document.querySelectorAll('.right--header--menu').forEach(function(sidebar){
-                  sidebar.style.display = "flex";
-                  overlay.style.display = "block";
-                
-                   })
-          })
-      
-      
-      
-      }
       
          
              
