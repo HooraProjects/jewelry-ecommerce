5a47cbfa2d6c7dc1e340f0b2cba3202fa86918f1
Hoora <88542810+HooraProjects@users.noreply.github.com>
2023-07-02 21:16:42 +0330

    create header.js and footer.js and import them

--- header.js
+++ header.js
@@ -1,17 +1,16 @@
-'use strict';
-export const header = `<header class="arma--header" role="banner">
- <nav class="right-header">
-<a class="menu-icon"><i class="fa-solid fa-bars"></i></a>
+'use strict'
+const myHeader = `<nav class="right-header">
+<a id="menu-icon" onclick="openMenu()"><i class="fa-solid fa-bars"></i></a>
 <ul class="right--header--menu">
   <li>
-    <a class="close-icon"><i class="fa-solid fa-xmark"></i></a>
+    <a id="close-icon" onclick="closeMenu()"><i class="fa-solid fa-xmark"></i></a>
   </li>
-  <li><a href="#" class="menu-item active" id="menu-item-1">آرما</a></li>
+  <li><a href="./index.html" class="menu-item active" id="menu-item-1">آرما</a></li>
   <li>
     <a href="./armaShop.html" class="menu-item" id="menu-item-2">فروشگاه</a>
   </li>
   <li>
-    <a href="./armaAboutUs.html" class="menu-item" id="menu-item-3"
+    <a href="./armaAbout.html" class="menu-item" id="menu-item-3"
       >درباره ما</a
     >
   </li>
@@ -23,17 +22,19 @@ export const header = `<header class="arma--header" role="banner">
 </ul>
 </nav>
 
-<img src="./assets/images/logo.png" id="logo--header" />
+<img src="./assets/images/logo.webp" id="logo--header" />
 <div class="left-header">
-<a href="#" class="menu-item-left-header"
-  ><i class="fa-solid fa-magnifying-glass"></i
-></a>
+ <div class="menu-item-left-header-1">
+   <a href="#">
+    <input type="text"  class="fas fa-search" placeholder="&#xf002">
+   </a>
+
+ </div>
 <a href="#" class="menu-item-left-header">
             <i class="fa-solid fa-user"></i>
           </a>
 <a href="#" class="menu-item-left-header"
   ><i class="fa-solid fa-cart-shopping"></i
 ></a>
-</div>
-</header>`;
-document.getElementsByTagName('header').innerHTML= header;
+</div>`
+document.getElementById('arma--header').innerHTML = myHeader;
--- script.js
+++ script.js
@@ -4,29 +4,7 @@ sideBar.style.display = "none";
 const overlay = document.querySelector('.overlay');
 overlay.style.display="none";
 
-const footer  = `<footer id="footer">
 
-<div class="wpb-wrapper">
-<div class="links-footer">
-<a href="#" class="links-footer-item">جواهرات آرما</a>
-<a href="#" class="links-footer-item">تضمین برگشت / معاوضه کالا</a>
-<a href="#" class="links-footer-item">باشگاه مشتریان</a>
-<a href="#" class="links-footer-item">درباره ما / تماس با ما</a>
-</div>
-
-<div><small>تمامی حقوق مادی و معنوی این وبسایت متعلق به جواهری آرما می‌باشد</small></div>
-</div>
-
-<div class="wpb-wrapper">
-<div class="join-club-footer-title"><p>عضویت در خبرنامه</p></div>
-<div class="join-club-footer">
-<input type="email" placeholder="ایمیل شما">
-<input type="submit" value="عضویت" class="btn-submit-footer" >
-</div>
-
-</div></footer>`;
-
-document.getElementById('footer').innerHTML= footer;
 
 const menuIcon = document.getElementById('menu-icon');
 const closeIcon = document.getElementById('close-icon');
